2fb85c06eab804e2fd1d3751fc84f5edc94eaf17
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-14 11:05:33 +0100

    arrow FUnc 1_4 fertig

---
+++ assets/js/main_js_arrow_func_1_4.js
@@ -0,0 +1,10 @@
+
+
+let hero = (heroName, heroPower, heroEnemy) => {
+    let name = `${heroName}`
+    let power = `${heroPower}`
+    let enemy = `${heroEnemy}`
+    console.log(`Mein Lieblingsheld ist: ${name}. Er hat die Fähigkeit: ${power} und sein größter Gegner ist: ${enemy}. Hihi`);
+}
+
+hero(`Victor`, `Süß zu sein`, `Daniel`)
